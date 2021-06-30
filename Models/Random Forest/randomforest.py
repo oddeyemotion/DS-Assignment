356c265670cb7c172cbeb1e4b3c01af816fdd536
Nguyễn Vinh <paddlestar16302@gmail.com>
2021-07-01 00:55:43 +0700

    Update - 7/1/2021 - 12:55 AM

---
+++ Models/Random Forest/randomforest.py
@@ -0,0 +1,109 @@
+from numpy import log, asarray
+from statsmodels.tsa.stattools import adfuller
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from scipy import stats
+import matplotlib.pyplot as plt
+import plotly.express as px  # install plotly
+from datetime import datetime, timedelta
+from sklearn.metrics import r2_score, mean_absolute_error
+from matplotlib.pyplot import figure
+import csv
+import calendar
+
+glo_bal = pd.read_csv('Global.csv')
+glo_bal2 = pd.read_csv('Global_wo_may.csv')
+glo_bal2['Active'] = glo_bal2['Confirmed'] - \
+    glo_bal2['Deaths'] - glo_bal2['Recovered']
+globalactive = pd.read_csv('GlobalActive.csv')
+
+
+
+A = globalactive
+size = int(len(A)*0.8)
+train, test = A[0:size], A[size:len(A)]
+
+regressor = RandomForestRegressor(n_estimators=100)
+
+
+# transform a time series dataset into a supervised learning dataset
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols = list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+	# put it all together
+	agg = pd.concat(cols, axis=1)
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg.values
+
+# split a univariate dataset into train/test sets
+
+
+def train_test_split(data, n_test):
+	return data[:-n_test, :], data[-n_test:, :]
+
+# fit an random forest model and make a one step prediction
+
+
+def random_forest_forecast(train, testX):
+	# transform list into array
+	train = asarray(train)
+	# split into input and output columns
+	trainX, trainy = train[:, :-1], train[:, -1]
+	# fit model
+	model = RandomForestRegressor(n_estimators=1000)
+	model.fit(trainX, trainy)
+	# make a one-step prediction
+	yhat = model.predict([testX])
+	return yhat[0]
+
+# walk-forward validation for univariate data
+
+
+def walk_forward_validation(data, n_test):
+	predictions = list()
+	# split dataset
+	train, test = train_test_split(data, n_test)
+	# seed history with training dataset
+	history = [x for x in train]
+	# step over each time-step in the test set
+	for i in range(len(test)):
+		# split test row into input and output columns
+		testX, testy = test[i, :-1], test[i, -1]
+		# fit model on history and make a prediction
+		yhat = random_forest_forecast(history, testX)
+		# store forecast in list of predictions
+		predictions.append(yhat)
+		# add actual observation to history for the next loop
+		history.append(test[i])
+		# summarize progress
+		print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+	# estimate prediction error
+	error = r2_score(test[:, -1], predictions)
+	return error, test[:, -1], predictions
+
+
+# load the dataset
+series = pd.read_csv('GlobalActive.csv', header=0, index_col=0)
+values = series.values
+# transform the time series data into supervised learning
+data = series_to_supervised(values, n_in=6)
+# evaluate
+mae, y, yhat = walk_forward_validation(data, 60)
+print('R2: %.3f' % mae)
+# plot expected vs predicted
+plt.plot(y, label='Expected')
+plt.plot(yhat, label='Predicted')
+plt.legend()
+plt.show()
