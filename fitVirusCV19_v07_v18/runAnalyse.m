8e7ebf634b6a4087ecadecbfbd822c9f8ccab95d
Arris Le <hang.le.3329@gmail.com>
2021-07-01 02:26:01 +0700

    T_T

--- fitVirusCV19_v07_v18/runAnalyse.m
+++
@@ -1,7 +0,0 @@
-%analyse data
-close all
-addpath('CV19')
-addpath('data')
-n = 10; % from n days back
-out = analyseCV19(@getDataFrance); %,n);
-
--- fitVirusCV19_v07_v18/runMeR.m
+++
@@ -1,6 +0,0 @@
-close all
-addpath('CV19')
-addpath('data')
-%Interpret data as removal cases
-res = fitVirusCV19R(@getDataAustria,'prn','on');
-
