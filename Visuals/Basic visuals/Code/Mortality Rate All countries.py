097553264fd3d7488c49d060a62e553acb0b5dec
Nguyễn Vinh <paddlestar16302@gmail.com>
2021-07-10 02:00:17 +0700

    plots 1 3 4 5

---
+++ Visuals/Basic visuals/Code/Mortality Rate All countries.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib as mpl
+import seaborn as sns
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot as plt
+
+mpl.style.use('seaborn-white')
+my_pallete = sns.set_palette(
+	['#E85B80', '#98C35A', '#FFAE6B', '#7FF9E3', '#D883FF', '#AB6E3B'])
+
+
+def parser(x):
+	return datetime.strptime('202'+x, '%Y-%m-%d')
+
+
+gl0bal = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Global%20(SPSS).csv',
+                  header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+india = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Highest%20Number%20of%20Infections%20Country%20~%20India%20(SPSS).csv',
+                 header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+us = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Highest%20Number%20of%20Infections%20Country%20~%20United%20States%20(SPSS).csv',
+              header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+brazil = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Highest%20Number%20of%20Infections%20Country%20~%20Brazil%20(SPSS).csv',
+                  header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+japan = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Asian%20Country%20~%20Japan%20(SPSS).csv',
+                 header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+korea = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Asian%20Country%20~%20South%20Korea%20(SPSS).csv',
+                 header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+vn = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Visuals/Basic%20visuals/Datasets%20of%20each%20country%20and%20Global/Asian%20Country%20~%20Vietnam%20(SPSS).csv',
+              header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+
+fig, ax = plt.subplots()
+ax.plot(gl0bal['Mortality_rate'], label="Global")
+ax.plot(india['Mortality_rate'], label="India")
+ax.plot(us['Mortality_rate'], label="US")
+ax.plot(brazil['Mortality_rate'], label="Brazil")
+ax.plot(japan['Mortality_rate'], label="Japan")
+ax.plot(korea['Mortality_rate'], label="Korea")
+ax.plot(vn['Mortality_rate'], label="Vietnam")
+ax.legend()
+ax.set_title('Confirmed, Death and Recovered cases of Global')
+plt.show()
