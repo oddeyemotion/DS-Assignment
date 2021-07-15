3405f70a70f6158c054311b374604bc232fb027b
tannghiepnguyen123 <tannghiepnguyen123@gmail.com>
2021-07-16 01:11:14 +0700

    Create globalcon.py

---
+++ Models/Version 3/ARIMA/globalcon.py
@@ -0,0 +1,130 @@
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+def parser(x):
+	return datetime.strptime('202'+x, '%Y-%m-%d')
+series = read_csv('https://raw.githubusercontent.com/ll-cooool-j/DS-Assignment/main/Datasets/Datasets%20for%20ARIMA%20(Only%20Date%20%2B%20Confirmed)/Global%20(Date%20%2B%20Confirmed).csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+print(series.head())
+series.plot()
+pyplot.show()
+
+series.max()
+
+series.min()
+
+from matplotlib import pyplot
+from pandas.plotting import autocorrelation_plot
+autocorrelation_plot(series)
+pyplot.show()
+
+# grid search ARIMA parameters for time series
+import warnings
+import pandas as pd
+from math import sqrt
+from pandas import read_csv
+from pandas import datetime
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+
+# evaluate an ARIMA model for a given order (p,d,q)
+def evaluate_arima_model(X, arima_order):
+  X = X.astype('float64')
+  # prepare training dataset
+  train_size= int(len(X) * 0.2)
+  point = int(len(X) * 0.8)
+  train, test = X[0:train_size], X[train_size:]
+  history = [x for x in train]
+
+  # make predictions
+  predictions = list()
+  for t in range(len(test)):
+    model = ARIMA(history, order=arima_order)
+    model_fit = model.fit()
+    yhat = model_fit.forecast()[0]
+    predictions.append(yhat)
+    history.append(test[t])
+  # calculate out of sample error
+  rmse = sqrt(mean_squared_error(test, predictions))
+  return rmse
+ 
+# evaluate combinations of p, d and q values for an ARIMA model
+def evaluate_models(dataset, p_values, d_values, q_values):
+	dataset = dataset.astype('float32')
+	best_score, best_cfg = float("inf"), None
+	for p in p_values:
+		for d in d_values:
+			for q in q_values:
+				order = (p,d,q)
+				try:
+					rmse = evaluate_arima_model(dataset, order)
+					if rmse < best_score:
+						best_score, best_cfg = rmse, order
+					print('ARIMA%s RMSE=%.3f' % (order,rmse))
+				except:
+					continue
+	print('Best ARIMA%s RMSE=%.3f' % (best_cfg, best_score))
+ 
+# load dataset
+idx = pd.date_range("2020-07-01", periods=335)
+series.index = idx.to_period()
+# evaluate parameters
+p_values = range(0, 5)
+d_values = range(0, 3)
+q_values = range(0, 3)
+warnings.filterwarnings("ignore")
+evaluate_models(series.values, p_values, d_values, q_values)
+
+# fit an ARIMA model and plot residual errors
+from pandas import DataFrame
+
+# fit model
+series = series.astype('float64')
+model = ARIMA(series, order=(1,1,0))
+model_fit = model.fit()
+# summary of fit model
+print(model_fit.summary())
+# line plot of residuals
+residuals = DataFrame(model_fit.resid)
+residuals.plot()
+pyplot.show()
+# density plot of residuals
+residuals.plot(kind='kde')
+pyplot.show()
+# summary stats of residuals
+print(residuals.describe())
+
+# evaluate an ARIMA model using a walk-forward validation
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import numpy as np
+import itertools
+from itertools import chain
+X = series.values.astype('float64')
+# prepare training dataset
+train_size= int(len(X) * 0.2)
+point = int(len(X) * 0.8)
+train, test = X[0:train_size], X[point:]
+history = [x for x in train]
+predictions = list()
+# walk-forward validation
+for t in range(len(test)):
+	model = ARIMA(history, order=(1,1,0))
+	model_fit = model.fit()
+	output = model_fit.forecast()
+	yhat = output[0]
+	predictions.append(yhat)
+	obs = test[t]
+	history.append(obs)
+	print('predicted=%f, expected=%f' % (yhat, obs))
+# evaluate forecasts
+rmse = sqrt(mean_squared_error(test, predictions))
+print('Test RMSE: %.3f' % rmse)
+# plot forecasts against actual outcomes
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.show()
+
